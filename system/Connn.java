7e35966a7c9063c76a2d79a105f37a598da1cace
K Eshwar Krishna <72182474+lazypandaa@users.noreply.github.com>
2024-11-08 14:30:57 +0530

    Add files via upload

---
+++ system/Connn.java
@@ -0,0 +1,16 @@
+package bank.management.system;
+
+import java.sql.*;
+
+public class Connn {
+    Connection connection;
+    Statement statement;
+    public Connn(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem", "root", "12345678");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
